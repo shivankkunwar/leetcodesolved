976a4f09cd8cf48e1c8dab974a3ff512fedfa344
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-05-11 18:11:28 +0530

    Time: 1 ms (99.68%), Space: 46.7 MB (9.89%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -3,26 +3,31 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int[][]dp=new int[m][n];
-        for(int i=0;i<m;i++){
+        for( int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-               if(i==0&&j==0)dp[i][j]= grid[i][j];
-                else{
-                    int right=Integer.MAX_VALUE;
-                
-                int down=Integer.MAX_VALUE;
-                if(i>0){
-                    right=dp[i-1][j];
-                }
-                if(j>0){
-                    down=dp[i][j-1];
-                }
-                dp[i][j]=Math.min(right,down)+grid[i][j];
-                
-                
+                dp[i][j]=-1;
             }
         }
-        }
-        return dp[m-1][n-1];
+        return check(grid,m-1,n-1,dp);
         
+        
+    }
+    public int check(int[][]grid,int i,int j,int [][]dp){
+        if(dp[i][j]!=-1)return dp[i][j];        
+        if(i<0||j<0)return Integer.MAX_VALUE;
+        int result;
+        if(i==0&&j==0)return grid[i][j];
+        else if(i==0){
+            result=grid[i][j]+check(grid,i,j-1,dp);
+        }
+        else if(j==0){
+            result=grid[i][j]+check(grid,i-1,j,dp);
+            
+        }else{
+             result=Math.min(check(grid,i-1,j,dp),check(grid,i,j-1,dp))+grid[i][j];
+       
+        }
+        
+        return dp[i][j]=result;
     }
 }
