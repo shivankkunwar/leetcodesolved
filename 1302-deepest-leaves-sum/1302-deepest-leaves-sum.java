0b410c87d4aeb4f4619f29a4e94f249c660cffea
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-05-16 01:08:48 +0530

    Time: 2 ms (94.33%), Space: 55.2 MB (64.95%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,62 +14,36 @@
  * }
  */
 class Solution {
-//     int sum=0;
-    
-//     int counter=0;
-//     public int deepestLeavesSum(TreeNode root) {
-//         int maxD=maxDepth(root);
-//        return sum(root,maxD);
-//     }
-//      public int maxDepth(TreeNode node)
-//     {
-//         if (node == null)
-//             return -1;
-//         else
-//         {
-//             /* compute the depth of each subtree */
-//             int lDepth = maxDepth(node.left);
-//             int rDepth = maxDepth(node.right);
-  
-//             /* use the larger one */
-//             if (lDepth > rDepth)
-//                 return (lDepth + 1);
-//              else
-//                 return (rDepth + 1);
-//         }
-//     }
-//     public int sum(TreeNode root,int maxD){
-//          if(root==null)return 0;
-//         counter++;
-//         sum(root.left,maxD);
-//         if(counter==maxD){
-          
-//             sum+=root.val;
-//         }
-        
-        
-//         sum(root.right,maxD);
-        
-//         return sum;
-//     }
-    
-    public int deepestLeavesSum(TreeNode root){
-        Queue<TreeNode> queue=new LinkedList();
-        queue.offer(root);
-        int level_sum=0;
-        while(!queue.isEmpty()){
-            level_sum=0;
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode current_node=queue.poll();
-                level_sum+=current_node.val;
-                if(current_node.left!=null)queue.offer(current_node.left);
-                if(current_node.right!=null)queue.offer(current_node.right);
+    int maxHeight=Integer.MIN_VALUE;
+    int counter=0;
+    int maxSum=0;
+   
+    public int deepestLeavesSum(TreeNode root) {
+        sum(root,counter);
+       return maxSum;
+    }
+   
+    public void sum(TreeNode root,int counter){
+         if(root==null)return ;
+        counter++;
+        sum(root.left,counter);
+        if(root.left==null&&root.right==null){
+            if(maxHeight<counter){
+                maxHeight=counter;
+                maxSum=root.val;
+            } else if(maxHeight == counter){
+                maxSum=maxSum+root.val;
             }
         }
-        return level_sum;
+        
+        
+        
+        sum(root.right,counter);
+        
+       
     }
     
+  
     
     
 }
