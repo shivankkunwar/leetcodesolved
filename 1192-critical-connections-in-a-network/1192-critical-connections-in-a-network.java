6d53d7d3a273d93367f492dcec7af70d87089ce3
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-05-18 23:49:06 +0530

    Time: 206 ms (48.71%), Space: 334.2 MB (36.58%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,30 +1,41 @@
 class Solution {
-      int[] disc, low;
-    int time = 1;
-    List<List<Integer>> ans = new ArrayList<>();
-    Map<Integer,List<Integer>> edgeMap = new HashMap<>();
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        disc = new int[n];
-        low = new int[n];
-        for (int i = 0; i < n; i++)
-            edgeMap.put(i, new ArrayList<Integer>());
-        for (List<Integer> conn : connections) {
-            edgeMap.get(conn.get(0)).add(conn.get(1));
-            edgeMap.get(conn.get(1)).add(conn.get(0));
-        }
-        dfs(0, -1);
-        return ans;
+    int[] disc = new int[n], low = new int[n];
+    List<Integer>[] adjList = new ArrayList[n];
+    List<List<Integer>> result = new ArrayList<>();
+    Arrays.fill(disc, -1); // use disc to track if visited (disc[i] == -1)
+    for (int i = 0; i < n; i++) adjList[i] = new ArrayList<>();    
+    for (int i = 0; i < connections.size(); i++) {
+      int from = connections.get(i).get(0);
+      int to = connections.get(i).get(1);
+      adjList[from].add(to);
+      adjList[to].add(from);
     }
-    public void dfs(int curr, int prev) {
-        disc[curr] = low[curr] = time++;
-        for (int next : edgeMap.get(curr)) {
-            if (disc[next] == 0) {
-                dfs(next, curr);
-                low[curr] = Math.min(low[curr], low[next]);
-            } else if (next != prev)
-                low[curr] = Math.min(low[curr], disc[next]);
-            if (low[next] > disc[curr]) 
-                ans.add(Arrays.asList(curr, next));
-        }
+    for (int i = 0; i < n; i++) {
+      if (disc[i] == -1) {
+        dfs(i, low, disc, adjList, result, i);
+      }
     }
+    return result;
+  }
+  
+  int time = 0; // time when discover each vertex
+
+  private void dfs(int u, int[] low, int[] disc, List<Integer>[] adjList, List<List<Integer>> result, int pre) {
+    disc[u] = low[u] = ++time; // discover u
+    for (int j = 0; j < adjList[u].size(); j++) {
+      int v = adjList[u].get(j);
+      if (v == pre) continue; // if parent vertex, ignore      
+      if (disc[v] == -1) { // if not discovered
+        dfs(v, low, disc, adjList, result, u);
+        low[u] = Math.min(low[u], low[v]);
+        if (low[v] > disc[u]) {
+          // u - v is critical, there is no path for v to reach back to u or previous vertices of u
+          result.add(Arrays.asList(u, v));
+        }
+      } else { // if v discovered and is not parent of u, update low[u], cannot use low[v] because u is not subtree of v
+        low[u] = Math.min(low[u], disc[v]);
+      }
+    }
+  }
 }
