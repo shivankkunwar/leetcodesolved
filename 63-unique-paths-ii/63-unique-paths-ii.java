74bee5ff122cb3ed21e83ae4b54c2928f3bffbe2
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-05-21 22:31:32 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (69.28%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,19 +1,33 @@
 class Solution {
-    public int count=0;
+    
     public int uniquePathsWithObstacles(int[][] grid) {
-     int [][] dp= new int[grid.length][grid[0].length];
-        return helper(grid,0,0,grid.length,grid[0].length,dp);
-
-    }
-    public  int helper( int [][] arr,int i,int j,int m,int n,int[][]dp){
-        if(i<0||j<0||i>=m||j>=n||arr[i][j]==1)return 0;
-        if(i==m-1&&j==n-1){
-
-            return 1;
+        int m= grid.length;
+        int n= grid[0].length;
+        
+        if(grid[0][0]==1)return 0;
+        
+        grid[0][0]=1;
+        
+        
+        for( int i=1;i<m;i++){
+            grid[i][0]=(grid[i][0]==0&&grid[i-1][0]==1)?1:0;
         }
-        if(dp[i][j]!=0)return dp[i][j];
-        int ans;
-        ans=helper(arr,i+1,j,m,n,dp)+helper(arr,i,j+1,m,n,dp);//down+right
-      return dp[i][j]=ans;
-}
+        for( int j=1;j<n;j++){
+            grid[0][j]=(grid[0][j]==0&&grid[0][j-1]==1)?1:0;
+        }
+        
+        for( int i=1;i<m;i++){
+            for( int j=1;j<n;j++){
+                if(grid[i][j]==0)
+                        grid[i][j]=grid[i-1][j]+grid[i][j-1];
+            
+                else
+                        grid[i][j]=0;
+            }
+        }
+        
+        return grid[m-1][n-1];
+        
+        
+    }
 }
