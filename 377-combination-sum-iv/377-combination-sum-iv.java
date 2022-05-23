82f3ff35b1534e54e46d53b7ea12f5f2640668fe
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-05-24 00:10:46 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (98.38%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,14 +1,28 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        int [] dp= new int [target+1];
-        dp[0]=1;
-        for(int i=1;i<target+1;i++){
-            for(int j:nums){
-                if(i>=j){
-                    dp[i]+=dp[i-j];
-                }
-            }
-        }
-        return dp[target];
+        int [] dp= new int[target+1];
+      
+        Arrays.fill(dp,-1);
+          dp[0]=1;
+        return helper(nums,target,dp);
+    }
+    public int helper( int [] arr,int tar,int[]dp){
+        if(dp[tar]!=-1){
+                return dp[tar];
+         }
+        int res=0;
+       for(int i=0;i<arr.length;i++){
+            if(arr[i]<=tar){
+                res+=helper(arr,tar-arr[i],dp);
+            }      
+            
+       
+       }
+        
+       dp[tar]=res;
+      return res;  
+        
+        
+        
     }
 }
