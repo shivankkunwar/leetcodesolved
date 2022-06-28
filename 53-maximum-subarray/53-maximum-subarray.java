e362a29221430f8cdf2970cbebedef3454505861
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-06-28 15:23:32 +0530

    Time: 4 ms (9.21%), Space: 74.3 MB (38.33%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,13 +1,15 @@
 class Solution {
-    public int maxSubArray(int[] nums) {
-        int sum=0;
-        int maxi=nums[0];//minimum one number exists 
-        for( int i=0;i<nums.length;i++){//linearly traverse 
-            sum+=nums[i];//add the current to the sum  
-            if(sum>maxi)maxi=sum;//if the sum is greater than the intial maximum value
-            if(sum<0)sum=0;//if the sum is less than 0 it wont be counted in the maximum answer;
-            
+    public int maxSubArray(int[] A) {
+           int n = A.length;
+        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = A[0];
+        int max = dp[0];
+        
+        for(int i = 1; i < n; i++){
+            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
         }
-    return maxi;
+        
+        return max;
     }
 }
