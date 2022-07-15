c39c17ef80039cd99f534783817e3f613647cb13
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-07-15 17:24:45 +0530

    Time: 4 ms (56.02%), Space: 46.8 MB (76.76%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,30 +1,17 @@
 class Solution {
+    private int n, m;
     public int maxAreaOfIsland(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int [][]  checker=new int [m][n];
-        int max=0;
-        for( int i=0;i<grid.length;i++){
-                for( int j=0;j<grid[i].length;j++){
-                    if(grid[i][j]==1&&checker[i][j]!=1) {
-                        
-                        max=Math.max(max,helper(grid,checker,i,j));
-                    }
-                    
-                }
-            }
-        return max;
+        int ans = 0;
+        n = grid.length;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < m; j++)
+                if (grid[i][j] > 0) ans = Math.max(ans, trav(i, j, grid));
+        return ans;
     }
-    public int helper(int[][]grid,int[][]checker,int i ,int j){
-        if(i<0||j<0||i>=grid.length||j>=grid[0].length){
-            return 0;    
-        }
-         if(grid[i][j]==1&&checker[i][j]!=1){
-             checker[i][j]=1;
-             return 1+helper(grid,checker,i+1,j)+helper(grid,checker,i-1,j)+helper(grid,checker,i,j+1)+helper(grid,checker,i,j-1);
-         }
-    
-    return 0;
+    private int trav(int i, int j, int[][] grid) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] < 1) return 0;
+        grid[i][j] = 0;
+        return 1 + trav(i-1, j, grid) + trav(i, j-1, grid) + trav(i+1, j, grid) + trav(i, j+1, grid);
     }
-    
 }
