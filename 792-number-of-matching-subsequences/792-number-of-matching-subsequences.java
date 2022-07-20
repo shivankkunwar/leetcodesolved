591ac81ed22fa0b81686240cfcbf2b5f5943b3b8
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-07-20 21:55:38 +0530

    Time: 31 ms (99.81%), Space: 59.1 MB (70.77%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,40 +1,41 @@
 class Solution {
-  
-        class Item {
+   public int numMatchingSubseq(String s, String[] words) {
+           Word[] heads = new Word[26];
+        for (int i = 0; i < 26; i++) heads[i] = new Word("", 0);
+        for (String word : words) {
+            Word head = heads[word.charAt(0) - 'a'];
+            Word newWord = new Word(word, 0);
+            newWord.next = head.next;
+            head.next = newWord;
+        }
+        int ans = 0;
+        for (char c : s.toCharArray()) {
+            Word curHead = heads[c - 'a'];
+            Word cur = curHead.next;
+            curHead.next = null;
+            while (cur != null) {
+                Word next = cur.next;
+                if (cur.index == cur.word.length() - 1) {
+                    ans++;
+                } else {
+                    cur.index++;
+                    Word nextHead = heads[cur.word.charAt(cur.index) - 'a'];
+                    cur.next = nextHead.next;
+                    nextHead.next = cur;
+                }
+                cur = next;
+            }
+        }
+        return ans;
+    }
+    
+    class Word {
         String word;
         int index;
-        public Item (String s, int i) {
-            word = s;
-            index = i;
+        Word next;
+        Word(String word, int index) {
+            this.word = word;
+            this.index = index;
         }
     }
-    public int numMatchingSubseq(String S, String[] words) {
-        Queue<Item>[] dict = new Queue[26];
-        for (int i = 0; i < dict.length; i++) {
-            dict[i] = new LinkedList<>();
-        }
-
-        for (String word :words) {
-            if (word.length() > 0) {
-                dict[word.charAt(0) - 'a'].add(new Item(word, 0));
-            }
-        }
-
-        int count = 0;
-        for (char c : S.toCharArray()) {
-            Queue<Item> queue = dict[c - 'a'];
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Item top = queue.remove();
-                top.index++;
-                if (top.index == top.word.length()) {
-                    count++;
-                } else {
-                    dict[top.word.charAt(top.index) - 'a'].add(top);
-                }
-            }
-        }
-
-        return count;
-    }
-    }
+}
