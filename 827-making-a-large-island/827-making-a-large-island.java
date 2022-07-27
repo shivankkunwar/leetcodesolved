87a82f1a7c150c156fd2353360302c6820dff06a
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-07-28 00:33:04 +0530

    Time: 405 ms (23.14%), Space: 265.5 MB (5.26%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.java
+++ 827-making-a-large-island/827-making-a-large-island.java
@@ -1,78 +1,53 @@
-class Solution {
-    
-    static int[] islandAreas = new int[500 * 500];
-    int islandIdx;
-    int rowLast;
-    int colLast;
-    
-    public int largestIsland(int[][] grid) {
-        islandIdx = 2;
-        islandAreas[2] = 0;
-        rowLast = grid.length - 1;
-        colLast = grid[0].length - 1;
 
-        // Find the area of all islands and assign each island an index number 2..n
-        for (int r = 0; r <= rowLast; r++) {
-            int[] row = grid[r];
-            for (int c = 0; c <= colLast; c++) {
-                if (row[c] == 1) {
-                    islandAreas[islandIdx] = 
-                        findIslandArea(grid, r, c);
-                    islandIdx++;
-                }
+class Solution {
+    public int N = 0;
+    public int largestIsland(int[][] grid) {
+        N = grid.length;
+        //DFS every island and give it an index of island
+        int index = 3, res = 0;
+        HashMap<Integer, Integer> area = new HashMap<>();
+        for (int x = 0; x < N; ++x) for (int y = 0; y < N; ++y)
+            if (grid[x][y] == 1) {
+                area.put(index, dfs(grid, x, y, index));
+                res = Math.max(res, area.get(index++));
             }
-        }
-        
-        // Check all the 0's in the grid to see how much area they could connect.
-        int maxArea = islandAreas[2];       // Just in case there are no 0's in the grid.
-        for (int r = 0; r <= rowLast; r++) {
-            int[] row = grid[r];
-            for (int c = 0; c <= colLast; c++) {
-                if (row[c] == 0) {
-                    int area = 1;
-                    int idxUp = 0;
-                    int idxLeft = 0;
-                    int idxDown = 0;
-                    if (r > 0) {
-                        int idx = grid[r - 1][c];
-                        if (idx > 0) {
-                            area += islandAreas[idx];
-                            idxUp = idx;
-                        }
+
+        //traverse every 0 cell and count biggest island it can conntect
+        for (int x = 0; x < N; ++x) for (int y = 0; y < N; ++y)
+            if (grid[x][y] == 0) {
+                HashSet<Integer> seen = new HashSet<>();
+                int cur = 1;
+                for (Pair<Integer, Integer> p : move(x, y)) {
+                    index = grid[p.getKey()][p.getValue()];
+                    if (index > 1 && !seen.contains(index)) {
+                        seen.add(index);
+                        cur += area.get(index);
                     }
-                    if (c > 0) {
-                        int idx = row[c - 1];
-                        if (idx > 0 && idx != idxUp) {
-                            area += islandAreas[idx];
-                            idxLeft = idx;
-                        }
-                    }
-                    if (r < rowLast) {
-                        int idx = grid[r + 1][c];
-                        if (idx > 0 && idx != idxUp && idx != idxLeft) {
-                            area += islandAreas[idx];
-                            idxDown = idx;
-                        }
-                    }
-                    if (c < colLast) {
-                        int idx = row[c + 1];
-                        if (idx > 0 && idx != idxUp && idx != idxLeft && idx != idxDown)
-                            area += islandAreas[idx];
-                    }
-                    if (area > maxArea)  maxArea = area;
                 }
+                res = Math.max(res, cur);
             }
-        }
-        return maxArea;
+        return res;
     }
-    
-    private int findIslandArea(int[][] grid, int r, int c) {
-        int area = 1;
-        grid[r][c] = islandIdx;
-        if (r > 0 && grid[r - 1][c] == 1)  area += findIslandArea(grid, r - 1, c);
-        if (c > 0 && grid[r][c - 1] == 1)  area += findIslandArea(grid, r, c - 1);
-        if (r < rowLast && grid[r + 1][c] == 1)  area += findIslandArea(grid, r + 1, c);
-        if (c < colLast && grid[r][c + 1] == 1)  area += findIslandArea(grid, r, c + 1);
-        return area;
+
+    public List <Pair<Integer, Integer>> move(int x, int y) {
+        ArrayList <Pair<Integer, Integer>> res = new ArrayList<>();
+        if (valid(x, y + 1)) res.add(new Pair<Integer, Integer>(x, y + 1));
+        if (valid(x, y - 1)) res.add(new Pair<Integer, Integer>(x, y - 1));
+        if (valid(x + 1, y)) res.add(new Pair<Integer, Integer>(x + 1, y));
+        if (valid(x - 1, y)) res.add(new Pair<Integer, Integer>(x - 1, y));
+        return res;
+    }
+
+    public boolean valid(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < N;
+    }
+
+    public int dfs(int[][] grid, int x, int y, int index) {
+        int area = 0;
+        grid[x][y] = index;
+        for (Pair<Integer, Integer> p : move(x, y))
+            if (grid[p.getKey()][p.getValue()] == 1)
+                area += dfs(grid, p.getKey(), p.getValue(), index);
+        return area + 1;
     }
 }
