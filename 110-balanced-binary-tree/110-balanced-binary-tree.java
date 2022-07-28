612733afe4611ea2c876c964a78fc2c65dbd354f
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-07-28 18:23:19 +0530

    Time: 1 ms (99.04%), Space: 44.1 MB (69.28%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -17,18 +17,19 @@ class Solution {
    public  boolean Flag=true;
     public boolean isBalanced(TreeNode root) {
       
-            heighter(root);
-       
-       return Flag; 
+           return heighter(root)==-1?false:true;
+
     }
     public int heighter(TreeNode root){
         if(root!=null){
             int left=heighter(root.left);
+            if(left==-1)return -1;
             int right= heighter(root.right);
+            if(right==-1)return -1;
             
-            if(Math.abs(left-right)>1)Flag=false;
+            if(Math.abs(left-right)>1)return -1;
             
-            return 1+Math.max(left,right);
+            else return 1+Math.max(left,right);
         }
         return 0;
     }
