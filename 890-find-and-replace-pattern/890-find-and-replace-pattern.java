f0ec1675ac6efcb8d93a6aa5c503b279ccfc790a
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-07-29 14:43:22 +0530

    Time: 4 ms (32.06%), Space: 43.2 MB (33.39%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -1,37 +1,26 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        List<String> ans= new ArrayList<>();
-        for(String str:words){
-            if(helper(str,pattern))ans.add(str);
-        }
+        List<String> ans = new ArrayList();
+        for (String word: words)
+            if (match(word, pattern))
+                ans.add(word);
         return ans;
     }
-    public boolean helper(String str,String pattern){
-        if(str.length()!=pattern.length())return false;
-        HashMap<Character,Character> check= new HashMap<>();
-    
-        for( int i=0;i<pattern.length();i++){
-           
-            if(!check.containsKey(pattern.charAt(i))){
-                   
-                    check.put(pattern.charAt(i),str.charAt(i));
-                    
-                }
-            else{
-                if(str.charAt(i)!=check.get(pattern.charAt(i))){
-                    return false;    
-                }
-            }
+
+    public boolean match(String word, String pattern) {
+        Map<Character, Character> M = new HashMap();
+        for (int i = 0; i < word.length(); ++i) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+            if (!M.containsKey(w)) M.put(w, p);
+            if (M.get(w) != p) return false;
         }
-        
-        boolean[] seen= new boolean[26];
-        for(char ch:check.values()){
-            if(seen[ch-'a'])return false;
-            seen[ch-'a']= true;
-            
-            
+
+        boolean[] seen = new boolean[26];
+        for (char p: M.values()) {
+            if (seen[p - 'a']) return false;
+            seen[p - 'a'] = true;
         }
         return true;
     }
-    
 }
