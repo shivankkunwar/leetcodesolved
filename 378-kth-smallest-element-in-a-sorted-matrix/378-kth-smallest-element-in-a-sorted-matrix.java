bc3717849cf0be7b53d0dd6d3546b6321f859ce8
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-08-02 21:30:52 +0530

    Time: 0 ms (100.00%), Space: 48.5 MB (86.01%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,16 +1,38 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-      int lo = matrix[0][0], hi = matrix[matrix.length - 1][matrix[0].length - 1] + 1;//[lo, hi)
-        while(lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            int count = 0,  j = matrix[0].length - 1;
-            for(int i = 0; i < matrix.length; i++) {
-                while(j >= 0 && matrix[i][j] > mid) j--;
-                count += (j + 1);
+        int m=matrix.length;
+        int  n=matrix[0].length;
+        
+        
+        int lo=matrix[0][0],hi=matrix[m-1][n-1];
+        while(lo!=hi){
+            int mid= lo+(hi-lo)/2;
+            int curr=binaryRank(matrix,mid);
+            if(curr<k){
+                lo=mid+1;
+            }else{
+                hi=mid;
             }
-            if(count < k) lo = mid + 1;
-            else hi = mid;
         }
         return lo;
+        
+        
+    }
+    public int binaryRank(int[][] arr,int target){
+        int m=arr.length;
+        int n= arr[0].length;
+        
+        int i=0;
+        int j=n-1;
+        int count=0;
+        while(i<m&&j>=0){
+           if(target>=arr[i][j]){
+               count+=j+1;
+               i++;
+           }else{
+               j--;
+           }
+        }
+        return count;
     }
 }
