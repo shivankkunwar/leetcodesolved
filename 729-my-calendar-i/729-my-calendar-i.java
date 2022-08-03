6db3892ce9c0575f03a5e6f7d18a0731160f6aa0
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-08-04 00:54:38 +0530

    Time: 23 ms (94.22%), Space: 55.2 MB (13.43%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,17 +1,15 @@
 class MyCalendar {
-      List<int[]> calendar;
+    private TreeMap<Integer, Integer> map = new TreeMap<>();
+  public MyCalendar() {
 
-    MyCalendar() {
-        calendar = new ArrayList();
-    }
+  }
 
-    public boolean book(int start, int end) {
-        for (int[] iv: calendar) {
-            if (iv[0] < end && start < iv[1]) return false;
-        }
-        calendar.add(new int[]{start, end});
-        return true;
-    }
+  public boolean book(int start, int end) {
+    Map.Entry<Integer, Integer> e = map.floorEntry(end - 1);
+    if (e != null && e.getValue() > start) return false;
+    map.put(start, end);
+    return true;
+  }
 }
 
 /**
