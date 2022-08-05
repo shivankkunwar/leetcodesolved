ee1c5610588c002dca358ca1dc11e96da37a0202
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-08-06 01:34:34 +0530

    Time: 1 ms (99.98%), Space: 51.6 MB (52.54%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -1,43 +1,59 @@
 class Solution {
-    public void solve(char[][] board) {
-        int m=board.length;
-        int n=board[0].length;
-        Queue<Integer> q= new LinkedList<>();
-         for( int i=0;i<m;i++){
-            if(board[i][0]=='O')q.add(i*n);
-            if(board[i][n-1]=='O')q.add(i*n+(n-1));
-        }
-        for( int i=0;i<n;i++){
-            if(board[0][i]=='O')q.add(i);
-            if(board[m-1][i]=='O')q.add((m-1)*n+(i));
-        }
-        int[][] dir=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
-        while(!q.isEmpty()){
-            int sz=q.size();
-            while(sz-->0){
-            int curr=q.poll();
-            
-            int row=curr/n;
-            int column=curr%n;
-            board[row][column]='B';
-            for( int i=0;i<dir.length;i++){
-                int nr=row+dir[i][0];
-                int nc=column+dir[i][1];
-                if(nr>=0&&nr<m&&nc>=0&&nc<n&&board[nr][nc]=='O'&&board[nr][nc]!='B'){
-                    q.add(nr*n+nc);
-                    board[nr][nc]='B';
-                }
-            }
-            }
-          
+       public void solve(char[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+        
+        
+        for(int i = 0 ; i <rows;i++)
+        {
+            if(board[i][0] == 'O') boundary(board,i,0);
+            if(board[i][cols-1] == 'O') boundary(board,i,cols-1);
         }
         
-        for( int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]=='O')board[i][j]='X';
-                if(board[i][j]=='B')board[i][j]='O';
+        for(int j = 0 ; j <cols;j++)
+        {
+            if(board[0][j] == 'O') boundary(board,0,j);
+            if(board[rows-1][j] == 'O') boundary(board,rows-1,j);
+        }
+        for(int i = 0 ; i< rows; i ++)
+        {
+            for(int j = 0 ; j < cols;j++)
+            {
+                if(board[i][j] == 'O') 
+                {
+                    board[i][j] ='X';
+                }
+                else if(board[i][j] == '*') 
+                {
+                    board[i][j] = 'O';
+                }
             }
         }
-       
+        
+        
+        
     }
+    
+    
+    void boundary(char[][] board , int i,int j )
+    {
+        if(i>=0 && i< board.length && j >= 0 && j< board[0].length && board[i]             [j] == 'O')
+        {
+             board[i][j] = '*';
+    
+            boundary(board,i-1,j);
+       
+        
+            boundary(board,i+1,j);
+        
+       
+            boundary(board,i,j+1);
+        
+       
+            boundary(board,i,j-1);
+        
+        
+        return ;
+    }
+}
 }
