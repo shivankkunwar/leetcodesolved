cbdabb31f021f432f8b04356c4c1134bad62c865
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-08-18 21:08:46 +0530

    Time: 138 ms (21.99%), Space: 85.7 MB (60.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,34 +1,17 @@
 class Solution {
     public int minSetSize(int[] arr) {
-        int n=arr.length;
-        int check=n/2;
-        Map<Integer,Integer> map= new HashMap<>();
-        
-        
-        for( int i=0;i<arr.length;i++){
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-            if(map.get(arr[i])>check){
-                return 1;
-            }
-        }
-        System.out.println(map);
-        Integer [] values= new Integer[map.size()];
-        int i=0;
-        for(int key:map.keySet()){
-            values[i++]=map.get(key);
-        }
-        Arrays.sort(values,Collections.reverseOrder());
-        int ans=0;
-        for(int val:values){
-            if(n<=check){
-                return ans;
-            }else{
-                n-=val;
+        Map<Integer, Integer> map = new HashMap<>();
+            for (int a : arr) map.put(a, map.getOrDefault(a, 0) + 1);
+
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+            for (int v : map.values()) maxHeap.offer(v);
+
+            int ans = 0, count = 0, half = arr.length / 2;
+            while (count < half) {
+                count += maxHeap.poll();
                 ans++;
-                }
-            
-        }
-        
-        return ans;
+            }
+
+            return ans;
     }
 }
