8ac0669169d61c3abda66bbd4aaca98783dbaa0e
shivankkunwar <66783424+shivankkunwar@users.noreply.github.com>
2022-08-28 02:30:04 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (7.45%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,13 +1,30 @@
 class Solution {
+    public int dir[][]= new int [][]{{1,0},{0,1}};
+    public int dp[][];
     public int uniquePaths(int m, int n) {
-        int dp[][]= new int [m+1][n+1];
+        dp= new int [m][n];
+        for( int[] arr:dp)
+        Arrays.fill(arr,-1);
         
-        return helper(m,n,0,0,dp);
+        return helper(m-1, n-1,0,0);
+        
+        
     }
-    public int helper(int m,int n,int i,int j,int [][]dp){
-        if(i<0||j<0||i>=m||j>=n)return 0;
-        if(i==m-1&&j==n-1)return 1;
-        if(dp[i][j]!=0)return dp[i][j];
-        return dp[i][j]=helper(m,n,i,j+1,dp)+helper(m,n,i+1,j,dp);
+    public int helper(int m,int n,int s ,int e){
+        
+        if(s>m||s<0||e>n||e<0)return 0;
+        if(s==m&&e==n)return 1;
+        
+        if(dp[s][e]!=-1)return dp[s][e];
+        
+        dp[s][e]=0;
+        
+        for( int i=0;i<dir.length;i++){
+            dp[s][e]=dp[s][e]+helper(m,n,s+dir[i][0],e+dir[i][1]);
+        }
+        return dp[s][e];
+        
     }
+   
+    
 }
